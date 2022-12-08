f428d7d379fb617c4cfe52ead1980454a3eb513e
Ben <benarcher1337@gmail.com>
2022-12-08 08:23:23 +0000

    forgot to include the actual file changes lol

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+
+var maxValue = 0;
+var currentValue = 0;
+
+foreach(var calorieLine in input)
+{
+    if (calorieLine == "")
+    {
+        if (currentValue > maxValue)
+        {
+            maxValue = currentValue;
+        }
+
+        currentValue = 0;
+
+        continue;
+    }
+
+    currentValue = currentValue + int.Parse(calorieLine.Trim());
+}
+
+Console.WriteLine(maxValue);
