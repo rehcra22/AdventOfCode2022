19f3b7d3411a518a5a644a005777583cac3d4bd5
Ben <benarcher1337@gmail.com>
2022-12-08 10:13:46 +0000

    completed day 1

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -1,23 +1,58 @@
-﻿var input = await File.ReadAllLinesAsync("input.txt");
-
-var maxValue = 0;
-var currentValue = 0;
-
-foreach(var calorieLine in input)
+﻿static async Task<int> GetMaxValue()
 {
-    if (calorieLine == "")
+    var input = await File.ReadAllLinesAsync("input.txt");
+
+    var maxValue = 0;
+    var currentValue = 0;
+
+    foreach (var calorieLine in input)
     {
-        if (currentValue > maxValue)
+        if (calorieLine == "")
         {
-            maxValue = currentValue;
+            if (currentValue > maxValue)
+            {
+                maxValue = currentValue;
+            }
+
+            currentValue = 0;
+
+            continue;
         }
 
-        currentValue = 0;
-
-        continue;
+        currentValue = currentValue + int.Parse(calorieLine.Trim());
     }
 
-    currentValue = currentValue + int.Parse(calorieLine.Trim());
+    return maxValue;
 }
 
-Console.WriteLine(maxValue);
+static async Task<int> GetSumOfHighestValues(int numberOfTopValues)
+{
+    var input = await File.ReadAllLinesAsync("input.txt");
+
+    var topValues = new int[numberOfTopValues];
+
+    var currentValue = 0;
+
+    foreach (var calorieLine in input)
+    {
+        if (calorieLine == "")
+        {
+            if (currentValue > topValues.Min())
+            {
+                var arrayIndex = Array.IndexOf(topValues, topValues.Min());
+
+                topValues[arrayIndex] = currentValue;
+            }
+
+            currentValue = 0;
+
+            continue;
+        }
+
+        currentValue = currentValue + int.Parse(calorieLine.Trim());
+    }
+
+    return topValues.Sum();
+}
+
+Console.WriteLine(await GetSumOfHighestValues(3));
