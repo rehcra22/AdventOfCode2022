2f730adb776eb5edad9d85cd769015ad38ab07c6
Ben <benarcher1337@gmail.com>
2022-12-08 13:10:53 +0000

    completed day 3 part 2

---
+++ AdventOfCode/AdventOfCode_Day3/Program.cs
@@ -0,0 +1,44 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+
+PartTwo(input);
+
+void PartOne(string[] input)
+{
+    const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var prioritySum = 0;
+
+    foreach (var packs in input)
+    {
+        var charToSplitOn = packs.Length / 2;
+
+        var firstCompartment = packs.Substring(0, charToSplitOn);
+        var secondCompartment = packs.Substring(charToSplitOn, charToSplitOn);
+
+        var duplicateValue = firstCompartment.Intersect(secondCompartment).FirstOrDefault();
+
+        prioritySum += (characters.IndexOf(duplicateValue) + 1);
+    }
+
+    Console.WriteLine(prioritySum);
+}
+
+void PartTwo(string[] input)
+{
+    const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var prioritySum = 0;
+
+    for (int i = 0; i < input.Length; i += 3)
+    {
+        var currentItem = input[i];
+        var nextItem = input[i + 1];
+        var lastItem = input[i + 2];
+
+        var badge = currentItem.Intersect(nextItem).Intersect(lastItem).FirstOrDefault();
+
+        prioritySum += (characters.IndexOf(badge) + 1);
+    }
+
+    Console.WriteLine(prioritySum);
+}
